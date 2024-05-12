48781947c8c8268091a1eb8e65e0b24a17f61e5e
Pcoi94 <pcoi94.mailpro@gmail.com>
2024-05-12 22:11:30 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless"
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), svelte()],
+  integrations: [tailwind(), mdx(), sitemap()],
+  output: 'server',
   adapter: vercel(),
 });
